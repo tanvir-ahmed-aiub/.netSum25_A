98c6339e7c936026168158a639b57fc7d7b73d2a
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-07-20 11:54:21 +0600

    Form Submission

---
+++ Form Submission/Form Submission/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Form_Submission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form_Submission.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student student) { 
+            
+            return View(student);
+        }
+        //public ActionResult Create(string Name, string Id, string Email) {
+        //    ViewBag.Name = Name;
+        //    ViewBag.Id = Id;
+        //    ViewBag.Email = Email;
+        //    return View();
+        //}
+        //public ActionResult Create(FormCollection fc)
+        //{
+        //    ViewBag.Name = fc["Name"];
+        //    ViewBag.Id = fc["Id"];
+        //    ViewBag.Email = fc["Email"];
+        //    return View();
+        //}
+    }
+}
