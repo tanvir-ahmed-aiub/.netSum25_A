06aac6c71cab37944e86cd1c8eb23fa5760aa712
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-07-23 12:43:14 +0600

    Intro EF

---
+++ IntroEF/IntroEF/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using IntroEF.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroEF.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        Sum25_AEntities db = new Sum25_AEntities();
+        public ActionResult Index()
+        {
+            var data = db.Students.ToList();
+            return View(data);
+        }
+        [HttpGet]
+        public ActionResult Create() { 
+            return View();
+        }
+        public ActionResult Create(Student s) { 
+            db.Students.Add(s);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id) {
+            var data = db.Students.Find(id); //searches with PK
+            return View(data);
+        }
+    }
+}
