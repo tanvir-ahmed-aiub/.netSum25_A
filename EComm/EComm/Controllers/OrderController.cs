eee848ec22169e5db2c8c5fe17fbc31b4bacdb30
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-08-02 15:41:53 +0600

    Update

---
+++ EComm/EComm/Controllers/OrderController.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using EComm.DTOs;
+using EComm.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EComm.Controllers
+{
+    public class OrderController : Controller
+    {
+        EComm_Sum25_AEntities db= new EComm_Sum25_AEntities();
+
+        Mapper GetMapper() {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Product,ProductDTO>().ReverseMap();
+            });
+            return new Mapper(config);
+        }
+        // GET: Order
+        public ActionResult Index()
+        {
+            var data = db.Products.ToList();
+            var products = GetMapper().Map<List<ProductDTO>>(data);
+            return View(products);
+        }
+        public ActionResult AddToCart(int id) {
+            var p = db.Products.Find(id);
+            var pr = GetMapper().Map<ProductDTO>(p);
+            pr.Qty = 1;
+
+            List<ProductDTO> cart = null;
+            if (Session["cart"] == null)
+            {
+                cart = new List<ProductDTO>();
+            }
+            else {
+                cart = (List<ProductDTO>)Session["cart"];
+            }
+            cart.Add(pr);
+            Session["cart"] = cart;
+            TempData["Msg"] = pr.Name+ " added to cart";
+            
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Increase(int id) {
+            var cart = (List<ProductDTO>)Session["cart"];
+            var p = (from pr in cart where pr.Id == id select pr).SingleOrDefault();
+            p.Qty++;
+            
+            return RedirectToAction("ShowCart");
+
+        }
+
+        public ActionResult Decrease(int id)
+        {
+            var cart = (List<ProductDTO>)Session["cart"];
+            var p = (from pr in cart where pr.Id == id select pr).SingleOrDefault();
+            p.Qty--;
+            return RedirectToAction("ShowCart"); ;
+        }
+
+        public ActionResult ShowCart() {
+            if (Session["cart"] == null)
+            {
+                TempData["Msg"] = "Cart is Empty";
+                return RedirectToAction("Index");
+            }
+            
+            var cart = (List<ProductDTO>)Session["cart"];
+            return View(cart);
+        }
+        [HttpPost]
+        public ActionResult PlaceOrder(double gTotal) {
+            var order = new Order() { 
+                Total = gTotal,
+                CustomerId = 1,
+                StatusId = 1,
+                Date = DateTime.Now,
+            };
+            db.Orders.Add(order);
+            db.SaveChanges();
+            var cart = (List<ProductDTO>)Session["cart"];
+            foreach (var item in cart)
+            {
+                var od = new OrderDetail() { 
+                    PId = item.Id,
+                    Qty = item.Qty,
+                    Price = item.Price,
+                    OId = order.Id
+                };
+                db.OrderDetails.Add(od);
+            }
+            db.SaveChanges();
+            Session["cart"] = null;
+            return RedirectToAction("Index");
+        }
+    }
+}
